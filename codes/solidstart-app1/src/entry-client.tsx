59678757057cb10d305e846f62020018c764399b
leo <leo@8u8.cc>
2025-10-17 11:41:11 +0400

    fix: 移除冗余项目

--- codes/solidstart-app1/src/entry-client.tsx
+++
@@ -1,7 +0,0 @@
-// @refresh reload
-import { mount, StartClient } from "@solidjs/start/client";
-
-mount(() => {
-  console.log("🚀 Client started：", performance.now());
-  return <StartClient />;
-}, document.getElementById("app")!);
